2f8f22df8e23acc392f6f9b050b034d821d86d45
Alan <alanmcruickshank@gmail.com>
2021-04-19 22:45:24 +0100

    basic state management

--- src/dbtease/state.py
+++ src/dbtease/state.py
@@ -1,5 +1,8 @@
 """Defines repositories which store state."""
 
+import json
+
+
 class DictStateRepository:
     """Basic state responsitory using an in memory dict."""
 
@@ -8,5 +11,32 @@ class DictStateRepository:
 
     def get_current_deployed(self):
         """Get the details of the currently deployed state."""
-        if "current_deployed" in self._state:
-            return False
+        return self._state.get("current_deployed", None)
+    
+    def set_current_deployed(self, commit_hash):
+        """Sets the details of the currently deployed state."""
+        self._state["currently_deployed"] = commit_hash
+
+
+class JsonStateRepository(DictStateRepository):
+    """Basic state responsitory using an in memory dict."""
+
+    def __init__(self, fname="state.json"):
+        self._fname = fname
+    
+    def _load_state(self):
+        return json.load(self.fname)
+    
+    def _save_state(self, state):
+        return json.dump(state, self.fname)
+    
+    def get_current_deployed(self):
+        """Get the details of the currently deployed state."""
+        state = self._load_state()
+        return state.get("current_deployed", None)
+    
+    def set_current_deployed(self, commit_hash):
+        """Sets the details of the currently deployed state."""
+        state = self._load_state()
+        state["currently_deployed"] = commit_hash
+        self._save_state(state)
